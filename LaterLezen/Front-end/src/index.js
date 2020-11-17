c8aceda63b4e3571a79a3156670d526cc4a92c78
glenn <hulscher12@gmail.com>
2020-11-17 15:41:06 +0100

    Solved a problem with materialize

--- LaterLezen/Front-end/src/index.js
+++ LaterLezen/Front-end/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './materialize.css';
-import './materialize.js';
 
 ReactDOM.render( <App />, document.getElementById('root') );
