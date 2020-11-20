91417b6dd58122445cdbc7457f2d198bf6c2e6a0
Ritse Huiskes <465083@student.saxion.nl>
2020-11-20 16:36:38 +0100

    edit

--- LaterLezen/laterlezer-extension/src/components/App.jsx
+++ LaterLezen/laterlezer-extension/src/components/App.jsx
@@ -7,7 +7,21 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       isLoggedIn: false,
+      username: '',
+      password: ''
     };
+  }
+
+  setUsername (value) {
+      this.setState(() => ({
+          username: value
+      }))
+  }
+
+  setPassword (value) {
+      this.setState(() => ({
+          password: value
+      }))
   }
 
   setLogin(value) {
@@ -19,11 +33,13 @@ export default class App extends React.Component {
   render() {
     console.log(this.state.isLoggedIn)
     const setIsLoggedIn = (c) => this.setLogin(c);
+    const setPass = (c) => this.setPassword(c);
+    const setUser = (c) => this.setUsername(c);
     return (
         <div>
             {this.state.isLoggedIn === true
-            ? <Article></Article>
-            : <Login setLogin={setIsLoggedIn}></Login>
+            ? <Article username={this.state.username} password={this.state.password}></Article>
+            : <Login setLogin={setIsLoggedIn} setPass={setPass} setUser={setUser} username={this.state.username} password={this.state.password}></Login>
             }
         </div>
     )
--- LaterLezen/laterlezer-extension/src/components/Login.jsx
+++ LaterLezen/laterlezer-extension/src/components/Login.jsx
@@ -1,16 +1,25 @@
 import React, {useState, useEffect} from "react";
 
 export default function Login(props) {
-    // const [username, setUsername]
     function handleLogin (e) {
         e.preventDefault();
         props.setLogin(true)
     }
 
+    function handleUsernameChange (e) {
+        e.preventDefault();
+        props.setUser(e.target.value)
+    } 
+
+    function handlePasswordChange (e) {
+        e.preventDefault();
+        props.setPass(e.target.value)
+    }
+
   return (
     <div>
-      <input type="text" placeholder="username/email" />
-      <input type="password" placeholder="password" />
+      <input type="text" placeholder="username/email" onChange={(e) => handleUsernameChange(e)} value={props.username}/>
+      <input type="password" placeholder="password" onChange={(e) => handlePasswordChange(e)} value={props.password}/>
       <button value="Log in" onClick={(e) => handleLogin(e)}>Log in</button>
     </div>
   );
