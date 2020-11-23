2656203a48b1416e7d5ec2c76f3d2db73d55965f
Kafune <kachung97@hotmail.com>
2020-11-23 15:56:51 +0100

    css changes tagslist

--- LaterLezen/laterlezer-extension/src/components/Login.jsx
+++ LaterLezen/laterlezer-extension/src/components/Login.jsx
@@ -20,9 +20,9 @@ export default function Login(props) {
   }
 
   return (
-    <div className="extension-bg">
-      <h1 className="login-title">LaterLezer</h1>
-      <div class="row login-form">
+    <div className="container extension-bg">
+      <h3 className="login-title">LaterLezer</h3>
+      <div className="row input-form">
         <input type="text" placeholder="username/email" className="input" onChange={(e) => setEmail(e.target.value)} value={props.username} />
         <input type="password" placeholder="password" className="input" onChange={(e) => setPassword(e.target.value)} value={props.password} />
         <button value="Log in" className="waves-effect waves-light btn" onClick={() => { handleLoginUser(email, password) }}>Log in</button>
--- LaterLezen/laterlezer-extension/src/components/TagsList.jsx
+++ LaterLezen/laterlezer-extension/src/components/TagsList.jsx
@@ -5,7 +5,7 @@ export default function TagList(props) {
   return (
     <div>
         {props.tags.map((element) => {
-          return <span key={element.title} className="tag retrieved-tag" onClick={() => props.handleTagSelect(element.title)}>
+          return <span key={element.title} className={"tag " + (props.selectedTags.includes(element.title) ? 'selected-tag' : 'retrieved-tag')} onClick={() => props.handleTagSelect(element.title)}>
             {element.title}
           </span>
         })}
